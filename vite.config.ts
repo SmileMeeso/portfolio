1115b1a0255b756b7cd4e9c2fb38ece96858c23a
misosoup <31465956+SmileMeeso@users.noreply.github.com>
2026-09-09 14:28:36 +0900

    feat: 스플리팅 방식 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ routesDirectory: './src/routes', generatedRouteTree: './src/routeTree.gen.ts' }),
+    TanStackRouterVite({
+      routesDirectory: "./src/routes",
+      generatedRouteTree: "./src/routeTree.gen.ts",
+      autoCodeSplitting: true,
+    }),
     react(),
   ],
-  assetsInclude: ['**/*.wasm'],
+  assetsInclude: ["**/*.wasm"],
   worker: {
-    format: 'es',
+    format: "es",
   },
-})
+});
